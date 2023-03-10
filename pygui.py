62bd131d40a0d83716237c2422b464081422dd77
Jornexe <Jornexe@gmail.com>
2023-03-10 13:10:23 +0100

    Update pygui.py
    working on setting up proper UI
    
    this was surprisingly annoying to work with.

--- pygui.py
+++ pygui.py
@@ -3,25 +3,31 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
+
+wdir = "C:/"
 def getdirlog():
     root = tk.Tk()
     root.withdraw()
-
-    default_path = "/path/with/ø/æ/å/characters"
-
-    options = {
-        'initialdir': default_path,
-    }
-
-    dirpath = filedialog.askdirectory(**options)
-
-    print(dirpath)
+    default_path = os.getcwd()
+    options = {'initialdir': default_path}
+    wdir = filedialog.askdirectory(**options)
+    return wdir
+def update_wdir():
+    global wdir
+    wdir = getdirlog()
 
 dpg.create_context()
 
 width, height = 600,600
 with dpg.window(tag="Prime", no_close=True, no_collapse=True, no_title_bar=True, no_move=True):
-    dpg.add_button(label="test",callback=lambda : print(getdirlog()))
+    dpg.add_listbox()
+    
+    dpg.add_button(label="select working directory", callback=lambda: update_wdir())
+        # dpg.add_text(label=wdir)
+    dpg.add_button(label="get cur dir", callback=lambda: print(wdir))
+with dpg.window(tag="FileList"):
+    with dpg.table():
+        dpg.add_table_column()
 
 dpg.create_viewport(title='File Name', width=width, height=height)
 
